0bc46498115f5e46d11f3e9541281cb353507263
pete-godwin <godwinapeter@gmail.com>
2026-08-13 16:25:47 +0100

    chore: initialize Spring Boot project

---
+++ src/main/java/com/petergodwin/box_delivery_service/BoxDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.petergodwin.box_delivery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petergodwin/box_delivery_service/BoxDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petergodwin.box_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
